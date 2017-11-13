9aa612415beeb3c82c349058b21ec870ea5c0895
Sebastian Tabares Amaya <sytabaresa@gmail.com>
2017-11-13 16:40:20 -0500

    version funcionand manual

--- src/main.cpp
+++ src/main.cpp
@@ -3,75 +3,61 @@
 #include "lookup_tables.h"
 
 #ifdef ATTINYx5
-#define POT1  A2
-#define SEL1  A3
+#define POT1  A3
+#define SEL1  A2
 #define SD    2
 #define H     1
 #define L     0
 #endif
 
-volatile uint8_t select = 0;
-volatile uint8_t analogValue = 0;
-uint8_t index = 0;
+volatile uint8_t pwmValue = 0;
+volatile uint8_t index = 0;
+uint8_t counter = 0;
 
-
-//TODO: usar ISR?
-//ISR(TIMER0_OVF_vect) {
-void updatePWM() {
-  uint8_t tmp;
-  index==166 ? index=0 : index++;
-
-  switch (select) {
-    case 0:
-    tmp = analogValue;
-    break;
-    case 1:
-    tmp = D1[index];
-    break;
-    case 2:
-    tmp = D2[index];
-    break;
-    default:
-      return;
+//void updatePWM() {
+ISR(TIMER1_OVF_vect) {
+  if (counter++ >= 32){ //10khz
+    counter = 0;
+    index == 166 ? index=0 : index++;
+    OCR1A = pwmValue;
   }
-  OCR1A = tmp;
 }
 
-
 void setup() {
-
   pinMode(POT1, INPUT);
   pinMode(SEL1, INPUT);
   pinMode(SD, OUTPUT);
-  pinMode(H, OUTPUT);
-  pinMode(L, OUTPUT);
+  pinMode(H, OUTPUT); // OC1A
+  pinMode(L, OUTPUT); // not OC1A
 
-  //SREG = _BV(7); // Global interruption enabled
+
+  digitalWrite(SD,LOW); // IR2112 active
+  SREG = _BV(7); // Global interruption enabled
 
   /*timer1 conf*/
   PLLCSR = _BV(LSM) | _BV(PLLE); //activates Low Speed Mode in PCK and enables it.
+  delay(1);
   while(!(PLLCSR & _BV(PLOCK))); //wait until PLL is in steady state.
-  PLLCSR = _BV(PCKE); //enable PCK source for timer1 (asynchronous mode).
+  PLLCSR |= _BV(PCKE); //enable PCK source for timer1 (asynchronous mode).
 
   //PWM freq = 20khz, page 88 in datasheet (PCK = 32Mhz)
-  //TIMSK = _BV(TOIE1); // interrupt enable when TCNT1 overflows (TCNT1 == OCR1A).
+  TIMSK = _BV(TOIE1); // interrupt enable when TCNT1 overflows (TCNT1 == OCR1A).
   OCR1C = 199; // top value for TCNT1 for PWM freq = 20khz, R = 7.6
   OCR1A = 100; // initial value of comparation
   TCCR1 = _BV(PWM1A) | _BV(COM1A0) | _BV(CS12); //activates complementary mode in A and 1:8 prescaler
 
   /*timer0 conf*/
-  OCR0A = 100; // 100 us
+  //OCR0A = 100; // 100 us
   //TIMSK |= _BV(TOIE0); // enable interruption when TCNTR0 == OCR0A
-  TCCR0A = _BV(WGM01); // CTC mode
-  TCCR0B = _BV(CS00); // prescaler 1:1.
+  //TCCR0A = _BV(WGM01); // CTC mode
+  //TCCR0B = _BV(CS00); // prescaler 1:1.
 }
 
+uint16_t tmp = 0;
+
 void loop() {
-  analogValue = analogRead(POT1) >> 2;
-  uint8_t tmp = analogRead(SEL1);
-  if (tmp <= 512) select = 0;
-  if (tmp > 512 && tmp <= 853) select = 1;
-  if (tmp > 853) select = 2;
-  updatePWM();
-  delayMicroseconds(100); //TODO: calibrar
+  tmp = analogRead(SEL1);
+  if (tmp <= 512) pwmValue = analogRead(POT1) >> 2;
+  else if (tmp > 512 && tmp <= 853) pwmValue = D1[index];
+  else pwmValue = D2[index];
 }
